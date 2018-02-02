4c9bfd8990846f5fa272e3ecc92fb4c2162e70c9
Michail Mishev <m.mishev1@gmail.com>
2018-02-02 19:04:41 +0200

    mvc

---
+++ ToDooo/Controller/TableViewController.swift
@@ -0,0 +1,161 @@
+//
+//  ViewController.swift
+//  ToDooo
+//
+//  Created by Michail Mishev on 22/1/18.
+//  Copyright © 2018 Michail Mishev. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let newItem = Item()
+        newItem.title = "Buy Milk"
+        itemArray.append(newItem)
+
+        let newItem2 = Item()
+        newItem2.title = "Have Lunch"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Make New App"
+        itemArray.append(newItem3)
+        
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
+            itemArray = items
+        }
+        
+    }
+
+    
+    //MARK - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+        
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        
+        //Ternary Operator:
+        // value = condition ? valueIfTrue : valueIFFalse
+        
+//        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+        cell.accessoryType = item.done ? .checkmark : .none     // <- even shorter
+        
+        //same as:
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+        
+        
+        return cell
+        
+    }
+    
+    
+    
+    
+    //MARK - TableView Delegate Method
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+//        print(itemArray[indexPath.row])
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
+
+        
+        tableView.reloadData()
+        
+        //the grey background diaspear after select it:
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New ToDooo Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks Add Item button on our UIAlert
+//            print(textField.text)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+
+}
+
+
+
---
+++ ToDooo/Data Model/Item.swift
@@ -0,0 +1,23 @@
+//
+//  Item.swift
+//  ToDooo
+//
+//  Created by Michail Mishev on 27/1/18.
+//  Copyright © 2018 Michail Mishev. All rights reserved.
+//
+
+import Foundation
+
+
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
+
+
+
+
+
